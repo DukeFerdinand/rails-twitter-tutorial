e73df743f905ccdd495b3bb0f65119912ceb93aa
Duke_Ferdinand <doug@dougflynn.dev>
2023-05-22 16:19:40 -0700

    Add user login via sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   has_secure_password
 
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, message: "Email is invalid" }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, message: "must be a valid email" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # GET /about
-  get "/about-us", to: "about#index", as: :about
+  get "/about", to: "about#index", as: :about
+
+  get "/signup", to: "registrations#new"
+  post "/signup", to: "registrations#create"
+
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy"
 
   root "main#index"
 end
