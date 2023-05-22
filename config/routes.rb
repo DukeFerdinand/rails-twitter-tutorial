1c71bf2306a5a9840fcae3500c7b818061c4009f
Duke_Ferdinand <doug@dougflynn.dev>
2023-05-22 16:38:36 -0700

    Add update password

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # GET /about
   get "/about", to: "about#index", as: :about
 
+  get "/password", to: "passwords#edit", as: :edit_password
+  patch "/password", to: "passwords#update"
+
   get "/signup", to: "registrations#new"
   post "/signup", to: "registrations#create"
 
